6be843b2bf0d7a0f7a2bb5eb5aad5bdba2162421
NikitaSergeevich <nvpetrochenko@edu.hse.ru>
2024-04-21 02:41:24 +0300

    ready

---
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -0,0 +1,33 @@
+type HangmanWordProps = {
+    reveal?: boolean
+    guessedLetters: string[]
+    wordToGuess: string
+}
+
+export const HangmanWord = ({ reveal = false, guessedLetters, wordToGuess }: HangmanWordProps) => {
+    return (
+        <div style={{
+            display: 'flex',
+            gap: '0.25em',
+            fontSize: '6rem',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            fontFamily: 'monospace'
+        }}>
+            {wordToGuess.split('').map((letter, index) => (
+                <span style={{
+                    borderBottom: '0.1em solid black'
+                }} key={index}>
+                    <span style={{
+                        visibility: guessedLetters.includes(letter.toLowerCase()) || reveal
+                            ? 'visible'
+                            : 'hidden',
+                        color: !guessedLetters.includes(letter) && reveal ? "red" : 'black'
+                    }}>
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    )
+}
